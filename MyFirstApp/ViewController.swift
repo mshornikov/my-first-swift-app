0bf5d53a24d323cb5753e6495dd44c763c680889
Mikhail Shornikov <mikhail.shornikov@mail.ru>
2023-06-08 18:08:19 +0300

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Михаил Шорников on 08.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
